c4c6f34cceb3b6d614eb7b573b3874fe498a990a
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-04-15 00:16:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @MCubeMediaOfficial</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : MCubeMedia Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @MCubeMediaOfficial    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
